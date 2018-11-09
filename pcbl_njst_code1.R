2ea697f6a8578d1c73ba18d4b9b7b24c05a7ff71
Dan Sloan <danielbsloan@gmail.com>
2018-11-08 20:16:27 -0700

    adding parallelization

---
+++ pcbl_njst_code1.R
@@ -0,0 +1,58 @@
+args <- commandArgs(TRUE)
+
+if (length(args) != 3) stop ("Required command line arugments are: input_tree_file outgroup_name output_name")
+
+library(phybase, warn.conflicts=FALSE)
+
+#define output file names by adding various extensions to the provided base names
+spp_tree = paste (args[3], "_species-tree.txt", sep="")
+output_nodes = paste (args[3], "_node_defs.txt", sep="")
+
+genetrees<-read.tree.string(file=args[1],format="phylip")
+
+#read.tree.string only extract taxa names from the first tree. Add any additional names from other trees 
+for (input_tree in genetrees$tree){
+	input_tree_nodes = read.tree.nodes(input_tree)
+	for (input_name in input_tree_nodes$names){
+		if(!input_name %in% genetrees$names){
+			genetrees$names = append(genetrees$names, input_name)
+		}
+	}
+}
+
+#define a species matrix. This assumes that there is one terminal for each species. NJst can accomodate mutliple terminals per species, but the following code would have to be modified to specify that in the matrix.
+numtax=length(genetrees$names)
+species.structure<-matrix(0,numtax,numtax)
+diag(species.structure)<-1
+
+#generate species tree with NJst based on all gene trees. 
+full_sptree = NJst(genetrees$tree, genetrees$names, genetrees$names, species.structure)
+
+#reroot species tree based on outgroup provided by user on the command line (must be a single species). Using ape functions to do this.
+full_sptree_obj = read.tree(text=full_sptree)
+full_sptree = write.tree(root(full_sptree_obj, args[2], resolve.root=TRUE))
+
+#print species tree to output file
+sink(spp_tree)
+cat (full_sptree)
+sink()
+
+#extract node information from full species tree
+full_sptree_nodes = read.tree.nodes(full_sptree)
+
+#print definition of nodes to output file
+sink(output_nodes)
+internal_node_count = 0
+#loop through each node, and obtain all taxa names that are descendents of that node
+for (node in 1:dim(full_sptree_nodes$nodes)[1]){
+	
+	full_node_indexes = offspring.species(node,full_sptree_nodes$nodes,numtax)
+	if (length(full_node_indexes) == 1 || length(full_node_indexes) >= numtax - 1) next
+	internal_node_count = internal_node_count + 1
+	full_node_names = c();
+	for (index_counter in 1:length(full_node_indexes)){
+		full_node_names[index_counter] = full_sptree_nodes$names[full_node_indexes[index_counter]]
+	}	
+	cat (internal_node_count, sort(full_node_names), "\n")
+}
+sink()
